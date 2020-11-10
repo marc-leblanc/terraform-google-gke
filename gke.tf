daf81892610d8cc251f8903611cbaa0ede3bc41c
Marc LeBlanc <mleblanc@localhost.localdomain>
2020-11-10 14:48:27 -0500

    fix linting errors

--- gke.tf
+++ gke.tf
@@ -90,6 +90,6 @@ output "gke_cluster_ca_cert" {
   value = google_container_cluster.kubernetes_cluster.master_auth.0.cluster_ca_certificate
 }
 
-output "gke_connection_string" { 
+output "gke_connection_string" {
   value = "gcloud container clusters get-credentials ${var.cluster_name} --zone ${var.location} --project ${var.project_id}"
 }
